b5861e802673266009430270667dcd1ec2326549
Arm <69001162+arskiir@users.noreply.github.com>
2021-12-09 20:33:48 +0700

    ??

---
+++ windi.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'windicss/helpers'
+
+export default defineConfig({
+  theme: {
+    colors: {
+      gray: {
+        100: '#F7FAFC',
+        900: '#1A202C',
+      },
+    },
+  },
+})
