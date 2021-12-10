eeb20d3287dfab3f779ff9a2cded17b7e6a0dc63
Arm <69001162+arskiir@users.noreply.github.com>
2021-12-11 00:11:49 +0700

    💄 snow tsParticles as a new background

--- src/particlesConfig.ts
+++ src/particlesConfig.ts
@@ -20,37 +20,78 @@ const createParticlesConfig = () => {
     fullScreen: {
       zIndex: -10,
     },
-    fpsLimit: 30,
-    // interactivity: {
-    //   events: {
-    //     onHover: {
-    //       enable: true,
-    //       mode: 'grab',
-    //     },
-    //   },
-    //   modes: {
-    //     grab: {
-    //       distance: 150,
-    //     },
-    //   },
-    // },
+    interactivity: {
+      events: {
+        onHover: {
+          enable: true,
+          mode: 'bubble',
+        },
+      },
+      modes: {
+        bubble: {
+          distance: 400,
+          duration: 0.3,
+          opacity: 1,
+          size: 4,
+        },
+      },
+    },
     particles: {
       links: {
-        blink: true,
-        distance: 150,
-        enable: true,
-        opacity: 0.5,
+        color: {
+          value: '#ffffff',
+        },
+        distance: 500,
+        opacity: 0.4,
+        width: 2,
       },
       move: {
+        attract: {
+          rotate: {
+            x: 600,
+            y: 1200,
+          },
+        },
+        direction: 'bottom',
         enable: true,
-        speed: 0.1,
-        outModes: 'out',
-      },
-      size: {
-        value: 0,
+        outModes: {
+          bottom: 'out',
+          left: 'out',
+          right: 'out',
+          top: 'out',
+        },
       },
       number: {
+        density: {
+          enable: true,
+        },
         value: getParticleNumber(),
+      },
+      opacity: {
+        random: {
+          enable: true,
+        },
+        value: {
+          min: 0.1,
+          max: 0.5,
+        },
+        animation: {
+          speed: 1,
+          minimumValue: 0.1,
+        },
+      },
+      size: {
+        random: {
+          enable: true,
+        },
+        value: {
+          min: 1,
+          max: 10,
+        },
+        animation: {
+          speed: 40,
+          minimumValue: 0.1,
+        },
       },
     },
   })
